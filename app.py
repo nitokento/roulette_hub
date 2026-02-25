c55d48ca83f1ea9213e93317c374b8aa0017c6b9
nitokento <nkent.1117@gmail.com>
2026-02-25 22:28:50 +0900

    施策です

--- app.py
+++ app.py
@@ -5,15 +5,11 @@ import random
 import os
 import time  
 
-#tryは失敗したときの保険なり#
+
 st.set_page_config(page_title="ダイスログ・システム", layout="centered")
-st.title("信頼してるけどｗ")
-try:
-    st.image("野球.jpg"with=150,caption="担当:澤村拓一")
-except:
-    st.error("画像ファイル'野球.jpg'が見つかりません。github確認して")
 
 
+st.title("信頼してるけど一旦回さね？笑")
 
 LOG_FILE = "ルーレッツ.csv"
 
@@ -21,79 +17,74 @@ LOG_FILE = "ルーレッツ.csv"
 if 'user_name' not in st.session_state:
     st.session_state.user_name = ""
 
+
 if not st.session_state.user_name:
-    st.info("最初に班名を登録してください")
-    name_input = st.text_input("名前（例：萩生田光一）")
+
+    st.info("最初に班名と現在の駅を登録してください")
+    name_input = st.text_input("名前（例：二戸班_高松駅)")
     if st.button("登録"):
         if name_input:
             st.session_state.user_name = name_input
             st.rerun()
         else:
-            st.warning("名前を入力してください")
+            st.warning("名前を入力しろって書いてあんだろ")
 else:
    
+    st.subheader(f"{st.session_state.user_name} での操作")
+
+    
     try:
-        st.sidebar.image("大学生中指無修正.JPG", width=50) 
+        st.image("sawamura.jpeg", width=150, caption="担当:澤村拓一")
+    except:
+        st.error("画像ファイル'sawamura.jpeg'が見つかりません。github確認して")
+
+    
+    try:
+        st.sidebar.image("nito.jpg", width=50) 
     except:
         st.sidebar.write("👤") 
 
     st.sidebar.write(f"ログイン中: **{st.session_state.user_name}**")
     
-    if st.sidebar.button("ログアウト（名前変更）"):
+    if st.sidebar.button("ログアウト"):
         st.session_state.user_name = ""
         st.rerun()
 
     
-    st.subheader(f"{st.session_state.user_name} さんの操作")
-    
-    if st.button("🎲 サイコロをぶん投げる"):
-        
+    if st.button("🚀宇宙開発(まわす)"):
         cut_in_container = st.empty() 
         try:
-            
-            gif_path = r"C:\Users\azuru\Desktop\ルーレット\澤村大暴投.gif"
-            
-           
+            gif_path = "澤村大暴投.gif"
             cut_in_container.image(gif_path, use_container_width=True)
-            
-            
             time.sleep(1.2) 
-            
-           
             cut_in_container.empty()
         except Exception as e:
             st.error(f"カットインの再生に失敗しました: {e}")
 
-        
         result = random.randint(1, 6)
-        
         now = datetime.now()
         time_stamp = now.strftime("%Y/%m/%d %H:%M:%S") 
 
-       
         new_log = {
             "発生時刻": time_stamp,
             "操作者": st.session_state.user_name,
             "出目": f"🎲 {result}"
         }
 
-        
         df_new = pd.DataFrame([new_log])
         df_new.to_csv(LOG_FILE, index=False, header=not os.path.exists(LOG_FILE), mode='a', encoding='utf_8_sig')
         
-        
-        st.balloons()
+       # st.balloons()#
         st.header(f"結果：{result} ") 
         st.success(f"【{time_stamp}】に記録しました！")
 
-   
+    
     st.divider()
     st.subheader("履歴一覧（最新順）")
     
     if os.path.exists(LOG_FILE):
         df_log = pd.read_csv(LOG_FILE)
         df_display = df_log.iloc[::-1] 
-        
         st.dataframe(df_display, use_container_width=True)
         
         csv = df_log.to_csv(index=False).encode('utf_8_sig')
